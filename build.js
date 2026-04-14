bf289d81bd98d1e55aeaec49306e0a5b0753efca
Majid Abarghooei <abarghooeimajid@gmail.com>
2026-04-14 20:14:29 +0200

    style: use Josefin Sans for post titles, add spacing above site-nav

--- build.js
+++ build.js
@@ -205,7 +205,7 @@ function baseTemplate({ title, description, image = '', content, url = '', type 
   <link rel="alternate" type="application/rss+xml" title="${escHtml(config.title)} RSS Feed" href="/feed.xml">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,400;0,500;0,700;1,400&family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,400;0,500;0,700;1,400&family=Inter:wght@400;500;600;700&family=Josefin+Sans:wght@400;600;700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="/assets/css/highlight.css">
   <link rel="stylesheet" href="/assets/css/style.css">
   <script type="application/ld+json">${JSON.stringify(jsonLd)}</script>
